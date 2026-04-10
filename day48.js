e1215b8d231fdb48e781d71cfca695287fecccb3
Manjunath G <manjunathmanjuu155@gmail.com>
2026-04-10 14:23:31 +0530

    day 48  javascript programs

---
+++ day48.js
@@ -0,0 +1,30 @@
+// let data = fetch("https://jsonplaceholder.typicode.com/users").then((d)=>{
+//     console.log(d)
+// })
+
+// let data = fetch("https://jsonplaceholder.typicode.com/users").then((d)=>{
+//     return(d.json())
+// }).then((finaldata)=>{
+//     console.log(finaldata)
+// })
+
+// let data = fetch("https://jsonplaceholder.typicode.com/posts").then((d)=>{
+//     return(d.json())
+// }).then((finaldata)=>{
+//     console.log(finaldata[0])
+// })
+
+// let data = fetch("https://jsonplaceholder.typicode.com/users").then((d)=>{
+//          return(d.json())
+// }).then((fd)=>{
+//     console.log(fd[0])
+// })
+
+let data2 = fetch("https://dummyjson.com/products").then((d)=>{
+        return(d.json())
+    
+}).then((fd)=>{
+    console.log(fd.products)
+})
+
+
